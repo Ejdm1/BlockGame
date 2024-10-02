929c29ce7cc05642f9aa8f3d60543845fe72baf7
Ejdm1 <kapsa.adam@seznam.cz>
2024-10-02 23:07:59 +0200

    triangle using glfw window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #define GLFW_INCLUDE_NONE
 #include <glfw/include/GLFW/glfw3.h>
 
@@ -13,6 +14,16 @@
 
 #include "backend/glfw_adapter.h"
 
+#include <simd/simd.h>
+
+#include <iostream>
+
+
+MTL::Device* device;
+MTL::RenderPipelineState* _pPSO;
+MTL::Buffer* _pVertexPositionsBuffer;
+MTL::Buffer* _pVertexColorsBuffer;
+
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     switch (key) {
@@ -22,20 +33,111 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     }
 }
 
-int main() {
+void buildShader() {
+    using NS::StringEncoding::UTF8StringEncoding;
 
+    const char* shaderSrc = R"(
+        #include <metal_stdlib>
+        using namespace metal;
+
+        struct v2f
+        {
+            float4 position [[position]];
+            half3 color;
+        };
+
+        v2f vertex vertexMain( uint vertexId [[vertex_id]],
+                               device const float3* positions [[buffer(0)]],
+                               device const float3* colors [[buffer(1)]] )
+        {
+            v2f o;
+            o.position = float4( positions[ vertexId ], 1.0 );
+            o.color = half3 ( colors[ vertexId ] );
+            return o;
+        }
+
+        half4 fragment fragmentMain( v2f in [[stage_in]] )
+        {
+            return half4( in.color, 1.0 );
+        }
+    )";
+
+    NS::Error* pError = nullptr;
+    MTL::Library* pLibrary = device->newLibrary( NS::String::string(shaderSrc, UTF8StringEncoding), nullptr, &pError );
+    if ( !pLibrary ) {
+        __builtin_printf( "%s", pError->localizedDescription()->utf8String() );
+        assert( false );
+    }
+
+    MTL::Function* pVertexFn = pLibrary->newFunction( NS::String::string("vertexMain", UTF8StringEncoding) );
+    MTL::Function* pFragFn = pLibrary->newFunction( NS::String::string("fragmentMain", UTF8StringEncoding) );
+        
+    MTL::RenderPipelineDescriptor* pDesc = MTL::RenderPipelineDescriptor::alloc()->init();
+    pDesc->setVertexFunction( pVertexFn );
+    pDesc->setFragmentFunction( pFragFn );
+    pDesc->colorAttachments()->object(0)->setPixelFormat( MTL::PixelFormat::PixelFormatBGRA8Unorm_sRGB );
+
+    _pPSO = device->newRenderPipelineState( pDesc, &pError );
+    if ( !_pPSO ) {
+        __builtin_printf( "%s", pError->localizedDescription()->utf8String() );
+        assert( false );
+    }
+
+    pVertexFn->release();
+    pFragFn->release();
+    pDesc->release();
+    pLibrary->release();
+}
+
+void buildBuffers()
+{
+    const size_t NumVertices = 3;
+
+    simd::float3 positions[NumVertices] =
+    {
+        { -0.8f,  0.8f, 0.0f },
+        {  0.0f, -0.8f, 0.0f },
+        { +0.8f,  0.8f, 0.0f }
+    };
+
+    simd::float3 colors[NumVertices] =
+    {
+        {  1.0, 0.3f, 0.2f },
+        {  0.8f, 1.0, 0.0f },
+        {  0.8f, 0.0f, 1.0 }
+    };
+
+    const size_t positionsDataSize = NumVertices * sizeof( simd::float3 );
+    const size_t colorDataSize = NumVertices * sizeof( simd::float3 );
+
+    MTL::Buffer* pVertexPositionsBuffer = device->newBuffer( positionsDataSize, MTL::ResourceStorageModeManaged );
+    MTL::Buffer* pVertexColorsBuffer = device->newBuffer( colorDataSize, MTL::ResourceStorageModeManaged );
+
+    _pVertexPositionsBuffer = pVertexPositionsBuffer;
+    _pVertexColorsBuffer = pVertexColorsBuffer;
+
+    memcpy( _pVertexPositionsBuffer->contents(), positions, positionsDataSize );
+    memcpy( _pVertexColorsBuffer->contents(), colors, colorDataSize );
+
+    _pVertexPositionsBuffer->didModifyRange( NS::Range::Make( 0, _pVertexPositionsBuffer->length() ) );
+    _pVertexColorsBuffer->didModifyRange( NS::Range::Make( 0, _pVertexColorsBuffer->length() ) );
+}
+
+
+int main() {
+    unsigned int count = 0;
     //glfw stuff
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* glfwWindow = glfwCreateWindow(800, 600, "Heavy", NULL, NULL);
+    GLFWwindow* glfwWindow = glfwCreateWindow(512, 512, "Heavy", NULL, NULL);
 
     //Metal Device
-    MTL::Device* device = MTL::CreateSystemDefaultDevice();
+    device = MTL::CreateSystemDefaultDevice();
 
     //Metal Layer
     CA::MetalLayer* metalLayer = CA::MetalLayer::layer()->retain();
     metalLayer->setDevice(device);
-    metalLayer->setPixelFormat(MTL::PixelFormat::PixelFormatBGRA8Unorm);
+    // metalLayer->setPixelFormat(MTL::PixelFormat::PixelFormatBGRA8Unorm_sRGB);
 
     //Adapt glfw window to NS Window, and give it a layer to draw to
     NS::Window* window = get_ns_window(glfwWindow, metalLayer)->retain();
@@ -46,6 +148,9 @@ int main() {
     //Command Queue
     MTL::CommandQueue* commandQueue = device->newCommandQueue()->retain();
 
+    buildShader();
+    buildBuffers();
+
     while (!glfwWindowShouldClose(glfwWindow)) {
         glfwPollEvents();
 
@@ -53,21 +158,31 @@ int main() {
 
         NS::AutoreleasePool* pool = NS::AutoreleasePool::alloc()->init();
         metalDrawable = metalLayer->nextDrawable();
-        MTL::CommandBuffer* commandBuffer = commandQueue->commandBuffer();
+
         MTL::RenderPassDescriptor* renderPassDescriptor = MTL::RenderPassDescriptor::alloc()->init();
+
         MTL::RenderPassColorAttachmentDescriptor* colorAttachment = renderPassDescriptor->colorAttachments()->object(0);
         colorAttachment->setTexture(metalDrawable->texture());
         colorAttachment->setLoadAction(MTL::LoadActionClear);
-        colorAttachment->setClearColor(MTL::ClearColor(0.75f, 0.25f, 0.125f, 1.0f));
+        colorAttachment->setClearColor(MTL::ClearColor(1.0f, 0.0f, 0.0f, 1.0f));
         colorAttachment->setStoreAction(MTL::StoreActionStore);
 
-        MTL::RenderCommandEncoder* renderCommandEncoder = commandBuffer->renderCommandEncoder(renderPassDescriptor);
-        renderCommandEncoder->endEncoding();
+        MTL::CommandBuffer* pCmd = commandQueue->commandBuffer();
+        MTL::RenderCommandEncoder* pEnc = pCmd->renderCommandEncoder( renderPassDescriptor );
 
-        commandBuffer->presentDrawable(metalDrawable);
-        commandBuffer->commit();
-        commandBuffer->waitUntilCompleted();
+        pEnc->setRenderPipelineState( _pPSO );
+        pEnc->setVertexBuffer( _pVertexPositionsBuffer, 0, 0 );
+        pEnc->setVertexBuffer( _pVertexColorsBuffer, 0, 1 );
+        pEnc->drawPrimitives( MTL::PrimitiveType::PrimitiveTypeTriangle, NS::UInteger(0), NS::UInteger(3) );
+
+        pEnc->endEncoding();
+        pCmd->presentDrawable(metalDrawable);
+        pCmd->commit();
+        pCmd->waitUntilCompleted();
+
         pool->release();
+        count++;
+        std::cout << count << std::endl;
     }
 
     commandQueue->release();
