27d6b681197292e3f4a2ab7d52e5d69edc6167ae
Ejdm1 <kapsa.adam@seznam.cz>
2024-10-04 19:05:13 +0200

    perspective cammera

---
+++ src/namespaces.hpp
@@ -0,0 +1,28 @@
+#include <simd/simd.h>
+
+namespace math
+{
+    constexpr simd::float3 add( const simd::float3& a, const simd::float3& b );
+    constexpr simd_float4x4 makeIdentity();
+    simd::float4x4 makePerspective();
+    simd::float4x4 makeXRotate( float angleRadians );
+    simd::float4x4 makeYRotate( float angleRadians );
+    simd::float4x4 makeZRotate( float angleRadians );
+    simd::float4x4 makeTranslate( const simd::float3& v );
+    simd::float4x4 makeScale( const simd::float3& v );
+}
+
+namespace shader_types
+{
+    struct InstanceData
+    {
+        simd::float4x4 instanceTransform;
+        simd::float4 instanceColor;
+    };
+
+    struct CameraData
+    {
+        simd::float4x4 perspectiveTransform;
+        simd::float4x4 worldTransform;
+    };
+}
